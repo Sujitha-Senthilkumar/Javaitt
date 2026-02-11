b3b3ecf19614eb6fea981b4ea891e6eaea1f4280
Sujitha-Senthilkumar <sujisenthil49@gmail.com>
2026-02-11 10:24:47 +0530

    Add files via upload
    This solution addresses the Move Zeroes problem (LeetCode 283) by rearranging an integer array to move all zeros to the end while preserving the original relative order of the non-zero elements.

---
+++ moveZeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i,k=0;
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            {
+                int temp=nums[i];
+                nums[i]=nums[k];
+                nums[k]=temp;
+                k++;
+            }
+        }
+    }
+}
